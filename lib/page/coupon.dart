d5e94e7d0f0153dd2f7d079239aaf3c214a08a1f
ChenMingMing <belovedyogurt@gmail.com>
2019-04-29 16:26:05 +0800

    add coupon page

--- lib/page/coupon.dart
+++ lib/page/coupon.dart
@@ -7,47 +7,79 @@ import 'package:flutter/material.dart';
 import '../router.dart';
 import 'base.dart';
 
-var Coupon = () => BodyPage.formBuild((BuildContext ctx, Map params, NoRoute router){
+var Coupon = () =>
+    BodyPage.formBuild((BuildContext ctx, Map params, NoRoute router) {
+      var _Coupons = (String title, num price, String timeEnd, String desc) =>
+          CardBox(
+              child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(title, textScaleFactor: 1.4, style: TextStyle(fontWeight: FontWeight.bold)),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(desc, textScaleFactor: 1.1),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(child: Text(timeEnd, textScaleFactor: 0.9)),
+                      SizedBox(
+                        child: Text("￥" + price.toString(), textScaleFactor: 1.5, style: TextStyle(color: Colors.red)),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ));
 
-  var _Coupons = (String title, num price, String timeEnd, String desc) =>
-      CardBox(
-        child: Stack()
-      );
+      var IOSNavBar = Offstage(
+          offstage: !Platform.isIOS,
+          child: GestureDetector(
+            onTap: () {
+              var nav = Navigator.of(ctx);
+              if (nav.canPop()) {
+                nav.pop();
+              }
+            },
+            child: Container(
+              padding: EdgeInsets.all(10),
+              width: MediaQuery.of(ctx).size.width - 20,
+              color: Color.fromRGBO(244, 244, 244, 1),
+              child: Row(
+                children: <Widget>[
+                  SvgPicture.asset("assets/svg/left.svg", width: 20)
+                ],
+              ),
+            ),
+          ));
 
-  var IOSNavBar = Offstage(
-      offstage: !Platform.isIOS,
-      child: GestureDetector(
-        onTap: (){
-          var nav = Navigator.of(ctx);
-          if(nav.canPop()){
-            nav.pop();
-          }
-        },
-        child: Container(
-          padding: EdgeInsets.all(10),
-          width: MediaQuery.of(ctx).size.width - 20,
-          color: Color.fromRGBO(244, 244, 244, 1),
-          child: Row(
-            children: <Widget>[
-              SvgPicture.asset("assets/svg/left.svg", width: 20)
-            ],
-          ),
-        ),
-      )
-  );
-
-  return SafeArea(child: Container(
-    padding: EdgeInsets.all(10),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Expanded(child: ListView(
+      return SafeArea(
+          child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            IOSNavBar,
-            ATitle("购物券"),
+            Expanded(
+                child: ListView(
+              children: <Widget>[
+                IOSNavBar,
+                ATitle("购物券"),
+                _Coupons("五一购物券", 99, "2019-11-1", "满 500 可用"),
+                _Coupons("圣诞礼券", 109, "2019-11-1", "无限制")
+              ],
+            ))
           ],
-        ))
-      ],
-    ),
-  ));
-}, noAnimate: false);
+        ),
+      ));
+    }, noAnimate: false);
